83f8f09deec3c5f72def7ab0b03a1099c50286bc
Pedro Muniz <pedrolehimuniz@gmail.com>
2024-04-03 14:28:21 -0300

    rebase at database and creation checkin table

--- src/routes/get-attendee-badge.route.ts
+++ src/routes/get-attendee-badge.route.ts
@@ -9,14 +9,28 @@ export async function getAttendeeBadge(app: FastifyInstance) {
     {
       schema: {
         params: z.object({ attendeeId: z.coerce.number() }),
-        response: {},
+        response: {
+          200: z.object({
+            badge: z.object({
+              name: z.string(),
+              email: z.string().email(),
+              eventId: z.string().uuid(),
+              eventTitle: z.string(),
+              checkInUrl: z.string().url(),
+            }),
+          }),
+        },
       },
     },
     async (request, reply) => {
       const { attendeeId } = request.params;
 
       const attendee = await prisma.attendee.findUnique({
-        select: { name: true, email: true },
+        select: {
+          name: true,
+          email: true,
+          event: { select: { id: true, title: true } },
+        },
         where: { id: attendeeId },
       });
 
@@ -24,7 +38,18 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         throw new Error("Attendee not found");
       }
 
-      return reply.send({ attendee });
+      const baseUrl = `${request.protocol}://${request.hostname}`;
+      const checkInUrl = new URL(`/attendee/${attendeeId}/check-in`, baseUrl);
+
+      return reply.send({
+        badge: {
+          name: attendee.name,
+          email: attendee.email,
+          eventId: attendee.event.id,
+          eventTitle: attendee.event.title,
+          checkInUrl: checkInUrl.toString(),
+        },
+      });
     }
   );
 }
